e8a423cfc1977c024b26c631265911c65daff10d
Breno Martins de Oliveira <brenojpmartins2002@gmail.com>
2022-04-25 16:22:55 -0300

    1stcommit

---
+++ app-notas/script.js
@@ -0,0 +1,48 @@
+const addBtn = document.getElementById('add');
+
+addBtn.addEventListener('click', () => {
+    addNewNote();
+});
+
+function addNewNote() {
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML = `
+    <div class="notes">
+    <div class="tools">
+    <button class="edit"><i class="fas fa-edit"> </i></button>
+    <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <div class="main hidden">
+    </div>
+    <textarea name="" id="" cols="30" rows=""></textarea>
+    </div>
+    `;
+
+    const notesEl = note.querySelector('.notes')
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden');
+    textArea.classList.toggle('hidden');
+});
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove(); 
+    })
+
+    textArea.addEventListener('input', (e) =>{
+    const { value } = e.target;
+
+    main.innerHTML = marked(value);
+});
+
+
+    document.body.appendChild(note);
+}
+
