5a53c951c007c06eff9b8f184ab96bedf0b90474
Breno Martins de Oliveira <brenojpmartins2002@gmail.com>
2022-04-26 22:01:49 -0300

    Commit
    app de notas

--- app-notas/script.js
+++ app-notas/script.js
@@ -1,10 +1,18 @@
 const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+    notes.forEach(note => {
+        addNewNote(note);
+    });
+}
+
 addBtn.addEventListener('click', () => {
     addNewNote();
 });
 
-function addNewNote() {
+function addNewNote(text = '') {
     const note = document.createElement('div');
     note.classList.add('note');
 
@@ -20,12 +28,15 @@ function addNewNote() {
     </div>
     `;
 
-    const notesEl = note.querySelector('.notes')
+    const notesEl = note.querySelector('.notes');
     const editBtn = note.querySelector('.edit');
     const deleteBtn = note.querySelector('.delete');
 
     const main = note.querySelector('.main');
     const textArea = note.querySelector('textarea');
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
 
     editBtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
@@ -34,15 +45,30 @@ function addNewNote() {
 
     deleteBtn.addEventListener('click', () => {
         note.remove(); 
+
+        updateLS();
     })
 
     textArea.addEventListener('input', (e) =>{
     const { value } = e.target;
 
     main.innerHTML = marked(value);
+
+    updateLS();
 });
 
 
     document.body.appendChild(note);
 }
 
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach(note => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
